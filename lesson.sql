369ddfae638d1d43c73382b81398753077a74407
mmkamalxtra <mmkamalxtra@gmail.com>
2026-09-23 12:33:54 +0000

    completed SELECT basis

--- lesson.sql
+++ lesson.sql
@@ -1,8 +1,27 @@
 /*
 Example lesson stub
 */
+--Some explanation
+SELECT   ps.PatientId,
+         ps.AdmittedDate,
+         ps.DischargeDate,
+         ps.Hospital,
+         ps.Ward,
+         ps.Ethnicity,
+         DATEDIFF(DAY, ps.AdmittedDate, ps.DischargeDate) AS LengthOfStay
+FROM     PatientStay AS ps
+WHERE    ps.Hospital IN ('kingston', 'PRUH')
+         AND --AND ps.Ward LIKE '%surgery'
+         ps.AdmittedDate BETWEEN '2024-2-28' AND '2024-03-01'
+ORDER BY LengthOfStay DESC, ps.AdmittedDate DESC;
 
-SELECT
-	*
-FROM
-	PatientStay ps ;
+SELECT   ps.Hospital,
+         ps.Ward,
+         COUNT(*) AS NumberOfPatients,
+         SUM(ps.tariff) AS TotalTariff,
+         MAX(ps.Tariff) AS BiggestTariff
+FROM     PatientStay AS ps
+GROUP BY ps.Hospital, ps.Ward
+HAVING  sum(ps.tariff) >= 10
+ORDER BY TotalTariff desc;
+
